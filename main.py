8e692e864f18d90272b263d66aca2b7592f831ac
AfoD3V <romanafonin@protonmail.com>
2023-08-15 11:27:00 +0200

    Adding optional filtering for the messages

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ INTENTS.message_content = True
 
 # Whitelist for Channels & Users
 CHANNELS = ["test"]
-USERS = ["_afo"]
+USERS = ["_afo_"]
 
 # Logs settings
 LOGS_FOLDER = "logs"
@@ -25,6 +25,10 @@ USER_COMMAND_LOGS = "user_command_logs.txt"
 # Time
 time_now = datetime.now()
 CURRENT_TIME = time_now.strftime("%H:%M:%S")
+
+# Bad Words Filter
+BAD_WORDS = ["slut", "whore", "cunt"]
+FILTERING_STATUS = True
 
 
 # Client Class
@@ -38,6 +42,13 @@ class MyClient(discord.Client):
         # System console print
         print(f"Message from {message.author}: {message.content}")
 
+        # Message filter
+        if FILTERING_STATUS:
+            for word in BAD_WORDS:
+                if message.content.lower().count(word) > 0:
+                    print("Offensive word - message deleted")
+                    await message.channel.purge(limit=1)
+
         # Message commands
         if str(message.channel) in CHANNELS and str(message.author) in USERS:
             # Test message
@@ -45,8 +56,10 @@ class MyClient(discord.Client):
                 await message.channel.send("test")
             # Counting on server users
             if message.content == "!user_count":
+                # Getting server id
                 id_ = client.get_guild(int(CONFIG["id"]["server_7sins"]))
                 await message.channel.send(f"Number of users: {id_._member_count}")
+        # Saving logs if unauthorized user is going to push command
         else:
             os.makedirs(LOGS_FOLDER, exist_ok=True)
             log_file_path = os.path.join(LOGS_FOLDER, USER_COMMAND_LOGS)
@@ -66,12 +79,10 @@ if __name__ == "__main__":
     # Run Client
     try:
         client.run(CONFIG["discord"]["token"])
-
     # If anny error occure - save logs
     except Exception as error_message:
         # Create folder if not exist
         os.makedirs(LOGS_FOLDER, exist_ok=True)
-
         # Append the error message to the 'logs.txt' file
         log_file_path = os.path.join(LOGS_FOLDER, ERROR_LOGS)
         with open(log_file_path, "a") as f:

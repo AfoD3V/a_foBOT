880444fe978da54fba1d5637c9789ee89a2526d9
Roman <98123424+AfoD3V@users.noreply.github.com>
2023-09-23 14:36:19 +0000

    Develope (#10)
    * Bot Core with base exception handling
    
    * Added current_time, user command logs handling, logs settings
    
    * Adding optional filtering for the messages
    
    * [Adding]Logs for message filtering, [Adding]Implementation of Secret Menu
    
    * [Adding]Exception handling for on_message and for accessing settings
    
    * [Adding]Key phrase on the discord chat can be hidden now with a spoiler tag, [Update]README.md description update
    
    * [Adding] Embedded !help command, [Updating] Secret Menu to be present as a embedded message
    
    * Implementing GitHub Actions workflows
    
    * [Modify] Extracting bot settings into independent module settings.py
    
    * [Adding] Docstring documentation and [Modifying] Body of `on_message` - condition tree is now extracted into single methods
    
    * Update deploy.yml
    
    * Update and rename deploy.yml to Deploy.yml
    
    * Deployment workflow rework
    
    * Making workflow rework changes
    
    * workflow rework
    
    * Update deploy.yml
    
    * Update Deploy.yml
    
    * workflows
    
    * adding fix
    
    * workflow fix
    
    * fix workflow
    
    * new workflow file
    
    * new workflow file
    
    * [Modify] Dockerfile updated, Secret's are no more accessable from config.ini [Adding] .dockerignore
    
    * [Modify] Modifying deploy.yml with a purpose of launching only when pull requested is closed
    
    * [Modify] Modifying workflows

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 from datetime import datetime, date
-from configparser import ConfigParser
 import discord
 
 """Secret Menu"""
@@ -7,17 +6,14 @@ SETTINGS_MENU = [
     False,  # 0 - Filtering
 ]
 
-
 """Bad Words Filter"""
 PROHIBITED_WORDS = ["test word"]
-
 
 """Time"""
 time_now = datetime.now()
 CURRENT_TIME = time_now.strftime("%H:%M:%S")
 date_today = date.today()
 TODAY_DATE = date_today.strftime("%d-%m-%y")
-
 
 """Logs Settings"""
 LOGS_FOLDER = "logs"
@@ -29,10 +25,6 @@ D_MESSAGES_LOGS = "deleted_messages_logs.txt"
 """Whitelist for Channels & Users"""
 CHANNELS = ["test"]
 USERS = ["_afo_", "a_foBOT#6965", "test_a_foBOT#8471"]
-
-"""Config File"""
-CONFIG = ConfigParser()
-CONFIG.read("config.ini")
 
 """Setting for data input (which kind of data bot can receive)"""
 INTENTS = discord.Intents.default()
